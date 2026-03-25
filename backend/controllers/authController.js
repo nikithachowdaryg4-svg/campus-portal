187b21576a7116d99a263962ff1018e6d2b1f936
Nikitha <nikithachowdaryg4@gmail.com>
2026-03-25 14:08:05 +0530

    Add detailed error logging to auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,17 +13,22 @@ const generateToken = (id) => {
 const authUser = async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await User.findOne({ username });
+  try {
+    const user = await User.findOne({ username });
 
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      username: user.username,
-      role: user.role,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401).json({ message: 'Invalid username or password' });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        username: user.username,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.error('Login Error:', error);
+    res.status(500).json({ message: 'Login internal error: ' + error.message });
   }
 };
 
